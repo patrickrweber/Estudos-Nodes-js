2113a9b29304dfd7b3293e105a3570f75e3a8a28
patrick <patrickrenanweber@gmail.com>
2023-06-27 21:59:10 -0300

    express e nodemon

---
+++ Módulos/calc.js
@@ -0,0 +1,12 @@
+var SomaFunc = require("./soma");
+var SubFunc = require("./sub");
+var MultiFunc = require("./multi");
+var DivFunc = require("./div");
+
+var a = 4;
+var b = 2;
+
+console.log(SomaFunc(a,b));
+console.log(SubFunc(a,b));
+console.log(MultiFunc(a,b));
+console.log(DivFunc(a,b));
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/html/index.html");
+});
+
+app.get("/sobre", function(req,res){
+    res.send("Minha página sobre o site");
+});
+
+app.get("/habitos", function(req,res){
+    res.sendFile(__dirname + "/html/habitos.html");
+});
+
+app.get("/blog", function(req, res){
+    res.send("Bem-vindo ao meu blog!");
+});
+
+app.get('/ola/:cargo/:nome', function(req, res){
+    res.send("<h1> Oii "+ req.params.nome + "</h1><h2> seu cargo é " + req.params.cargo + "</h2>");
+});
+
+app.listen(8081, function(){
+    console.log("Servidor rodando na url http://localhost:8081");
+});
